2876cde930050fdb1069abb77a990148655bed81
Ivana Djurkovic <ivanica0607@gmail.com>
2020-04-13 04:06:51 +0200

    Updates

---
+++ precache-manifest.3af6aa4d372712bbd1108d1ae4284067.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfe5cb303f86f8b8b7a9260f7cd34d1",
+    "url": "/final-project/index.html"
+  },
+  {
+    "revision": "1bb82d9b7446549ce9e4",
+    "url": "/final-project/static/css/main.b6559b2f.chunk.css"
+  },
+  {
+    "revision": "043a919ca8d2a2834eca",
+    "url": "/final-project/static/js/2.382120fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/final-project/static/js/2.382120fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb82d9b7446549ce9e4",
+    "url": "/final-project/static/js/main.38f020df.chunk.js"
+  },
+  {
+    "revision": "748e763e653345b3a21d",
+    "url": "/final-project/static/js/runtime-main.3f41d506.js"
+  },
+  {
+    "revision": "d0fc638fed533be968b181e3c4de2810",
+    "url": "/final-project/static/media/ExtraSales.d0fc638f.ttf"
+  },
+  {
+    "revision": "be066ad9cf260d492b26b3f002f25d5a",
+    "url": "/final-project/static/media/ExtraSalesBlank.be066ad9.ttf"
+  },
+  {
+    "revision": "3c1af9e9c056b338159decf28c9a32e2",
+    "url": "/final-project/static/media/hot-grill-demo.3c1af9e9.ttf"
+  }
+]);
